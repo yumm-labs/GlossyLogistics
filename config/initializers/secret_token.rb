752a48ec2243cb2613f58e0d83535f6c0556069d
Sathish Veera <sathishd.veera@gmail.com>
2012-03-11 21:54:18 +0530

    Added skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlossyLogistics::Application.config.secret_token = '804114bc45c3c9908f608c9282a5a326fb0357240e81d403e63705d84d786334f750fb4631db75ca0d468c006a5168a8ac7ac0363d1d6c21e57af9393fcb9d65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GlossyLogistics::Application.config.session_store :cookie_store, key: '_glossy_logistics_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GlossyLogistics::Application.config.session_store :active_record_store
