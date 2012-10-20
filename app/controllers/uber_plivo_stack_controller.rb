10e79d7be1d88a4d26b53afebaf84aa313c77757
bethink <manojs.nitt@gmail.com>
2012-10-20 12:39:36 +0530

    Plivo stack 1

--- app/controllers/uber_plivo_stack_controller.rb
+++ app/controllers/uber_plivo_stack_controller.rb
@@ -1,28 +1,28 @@
 class UberPlivoStackController < ApplicationController
 
   def answer_url
-    msg = "====== in answer url ====="
+    msg = "====== in answer url ===== #{params.inspect} ==========="
     puts msg
     Rails.logger.info msg
     render :text => "====== in answer url ===== #{params.inspect} ==========="
   end
 
   def fall_back_answer_url
-    msg = "====== in fall back answer url ====="
+    msg = "====== in fall back answer url =====#{params.inspect} ==========="
     puts msg
     Rails.logger.info msg
     render :text => "====== in fall back answer url ===== #{params.inspect} ==========="
   end
 
   def hangup_url
-    msg = "====== in hangup url ====="
+    msg = "====== in hangup url =====#{params.inspect} ==========="
     puts msg
     Rails.logger.info msg
     render :text => "====== in hangup url ===== #{params.inspect} ==========="
   end
 
   def message_url
-    msg = "====== in message url ====="
+    msg = "====== in message url =====#{params.inspect} ==========="
     puts msg
     Rails.logger.info msg
     render :text => "====== in message url ===== #{params.inspect} ==========="
