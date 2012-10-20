3fdd0f54ce13872f0506aba04ee04e49f5c27b90
bethink <manojs.nitt@gmail.com>
2012-10-20 14:52:01 +0530

    Plivo stack 1

--- app/controllers/uber_plivo_stack_controller.rb
+++ app/controllers/uber_plivo_stack_controller.rb
@@ -1,14 +1,26 @@
 class UberPlivoStackController < ApplicationController
 
+  # def speak(params={})
+  # call_uuid = params.delete('call_uuid')
+  # return request('POST', "/Call/#{call_uuid}/Speak/", params)
+  # end
+  def make_call
+    rest_api = Plivo::RestAPI.new('MAMWI4ZJYWMWI4YZJLZG', 'NzczYmU0NmI2NjNkYTg3ZTY4NTg3NzBmYmZkOWZl')
 
-      # def speak(params={})
-          # call_uuid = params.delete('call_uuid')
-          # return request('POST', "/Call/#{call_uuid}/Speak/", params)
-      # end
+    params = {'to' => '919742395094',
+      'from' => '443308080226',
+      'answer_url' => 'http://glossylogistics.heroku.com/uber_plivo_stack/answer_url',
+      'answer_method' => 'POST',
+      'hangup_url' => 'http://glossylogistics.heroku.com/uber_plivo_stack/hangup_url',
+      'hangup_method' => 'POST'
+    }
+
+    response = rest_api.make_call(params)
+  end
 
   def answer_url
 
-  # {
+    # {
     # "Direction"=>"inbound",
     # "From"=>"919742395094",
     # "CallerName"=>"919742395094",
@@ -16,22 +28,22 @@ class UberPlivoStackController < ApplicationController
     # "To"=>"443308080226",
     # "CallUUID"=>"d1569f32-1a85-11e2-8045-cbfbb0dc5cec",
     # "Event"=>"StartApp", "action"=>"answer_url", "controller"=>"uber_plivo_stack"
-  # }
+    # }
 
     Rails.logger.info "====== in answer url ===== #{params.inspect} ==========="
     Rails.logger.info "====== #{params['CallUUID'].inspect} ============"
-    
+
     rest_api = Plivo::RestAPI.new('MAMWI4ZJYWMWI4YZJLZG', 'NzczYmU0NmI2NjNkYTg3ZTY4NTg3NzBmYmZkOWZl')
 
     args = {
-      'call_uuid' => params['CallUUID'],
-      'text' => 'Hi... welcome to mentii'
+      :call_uuid => params['CallUUID'],
+      :text => 'Hi... welcome'
     }
 
     resp = rest_api.speak(args)
-    
+
     Rails.logger.info "===== Speak Response ===== #{resp.inspect} ==========="
-    
+
     render :text => "====== in answer url ===== #{params.inspect} ==========="
   end
 
